da0f8214184bb3e26a94b207ac174b3bf1da9dcf
ParadoxalGlitch <pereztobarradavid@gmail.com>
2023-04-21 13:24:23 +0200

    Started the repo

---
+++ prueba.js
@@ -0,0 +1,12 @@
+// Imprimir en consola
+
+console.log(19);
+
+console.log('Porygon-Z');
+
+console.log('Porygon-Z'.length);
+
+console.log('Porygon-Z'.toUpperCase());
+
+console.log('Porygon-Z'.startsWith('P'));
+console.log('Porygon-Z'.startsWith('H'));
